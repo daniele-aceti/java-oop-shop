1bd72e46c8cf964c955c561968141af9c2dc90b6
daniele-aceti <daniele.aceti@hotmail.it>
2025-01-29 21:28:07 +0100

    feat: prodotto

---
+++ src/Prodotto.java
@@ -0,0 +1,18 @@
+public class Prodotto {
+    
+    public int code;
+
+    public String name;
+
+    public String description;
+
+    public int price;
+
+    public int iva;
+
+    
+
+
+
+    }
+
